58444f6829598bed1a12776d41fa9b2d0d826e99
joekingsleymukundi <joekingsleymukundi@gmail.com>
2021-04-28 13:05:57 -0700

    err check 2

--- conrollers/apis_logic/switchlogic.js
+++ conrollers/apis_logic/switchlogic.js
@@ -4,29 +4,28 @@ const completeTaskApiController = require("../apis_routes/completetask")
 const dashboardApiController = require("../apis_routes/dashboard")
 const User = require("../../db_conn/models/users")
 const balanceUpdate = (app,req,res,found,task,bal,model,today)=>{
+    dashboardApiController(app)
+    completeTaskApiController(app)
     if(found.userComplete.includes(req.user.id)){
-        completeTaskApiController(app)
         req.flash("message","you already completed this")
         res.redirect(`/tasks/${task}`)
    }
     else{
-        dashboardApiController(app)
-        console.log("yess")
-        // User().updateOne({_id:req.user.id},{balance:bal},(err)=>{
-        //     if (err) {
-        //         console.log(err)
-        //     } else {
-        //             model().updateOne({date:today},{$push:{userComplete:req.user.id}},(err)=>{
-        //                 if (err) {
-        //                     console.log(err)
-        //                 } else {
-        //                     earningMail(req.user.email,req.user.username,0)
-        //                     res.redirect("/dashboard")
-        //                 }
-        //             })
+        User().updateOne({_id:req.user.id},{balance:bal},(err)=>{
+            if (err) {
+                console.log(err)
+            } else {
+                    model().updateOne({date:today},{$push:{userComplete:req.user.id}},(err)=>{
+                        if (err) {
+                            console.log(err)
+                        } else {
+                            earningMail(req.user.email,req.user.username,0)
+                            res.redirect("/dashboard")
+                        }
+                    })
                 
-        //     }
-        // })
+            }
+        })
       
     }
 }
