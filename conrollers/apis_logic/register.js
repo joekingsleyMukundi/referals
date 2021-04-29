17f4b286c16955a35de051405acdcad611b4baa8
joekingsleymukundi <joekingsleymukundi@gmail.com>
2021-04-29 02:17:19 -0700

    more correction

--- conrollers/apis_logic/register.js
+++ conrollers/apis_logic/register.js
@@ -5,53 +5,59 @@ const time = require("../../functions/dateTime")
 const {registerMail} = require("../../mails/sendmail")
 const date = require("../../utils/date")
 const registerApiLogic = (req,res)=>{
-    const otp = otpGen()
-    const regTime = time()
-    User().register({
-        username:req.body.username,
-        fullname:req.body.fullname,
-        phone:req.body.phone,
-        email:req.body.email,
-        userProfilePic:req.file.path,
-        otp:otp,
-        verified:false,
-        regDate:regTime,
-        balance:0,
-        package:'Basic',
-        pendingWithdrowal:0,
-        uplineId:req.body.uplineid,
-    },req.body.password,(err,user)=>{
-        if(err){
-            console.log(err)
-        }else{
-            registerMail(req.body.email,req.body.username,otp)
-            if (req.body.uplineid==="") {
-                passport.authenticate("local")(req,res,()=>{
-                    req.flash("message","succefully registered please check your email and get the otp")
-                    res.redirect("/otp")
-                })
+    if (req.body.password == req.body.confirmpassword) {
+        const otp = otpGen()
+        const regTime = time()
+        User().register({
+            username:req.body.username,
+            fullname:req.body.fullname,
+            phone:req.body.phone,
+            email:req.body.email,
+            userProfilePic:req.file.path,
+            otp:otp,
+            verified:false,
+            regDate:regTime,
+            balance:0,
+            package:'Basic',
+            pendingWithdrowal:0,
+            uplineId:req.body.uplineid,
+        },req.body.password,(err,user)=>{
+            if(err){
+                console.log(err)
             }else{
-                passport.authenticate("local")(req,res,()=>{
-                    const today = date()
-                    const downlinedata = {
-                        date:today,
-                        id:req.user.id,
-                    }
-                    User().updateOne({_id:req.body.uplineid},{$push:{downlineIds:downlinedata}},(err)=>{
-                        if (err) {
-                            console.log(err)
-                        }else{
-                            req.flash("message","succefully registered please check your email and get the otp")
-                            res.redirect("/otp")
+                registerMail(req.body.email,req.body.username,otp)
+                if (req.body.uplineid==="") {
+                    passport.authenticate("local")(req,res,()=>{
+                        req.flash("message","succefully registered please check your email and get the otp")
+                        res.redirect("/otp")
+                    })
+                }else{
+                    passport.authenticate("local")(req,res,()=>{
+                        const today = date()
+                        const downlinedata = {
+                            date:today,
+                            id:req.user.id,
                         }
+                        User().updateOne({_id:req.body.uplineid},{$push:{downlineIds:downlinedata}},(err)=>{
+                            if (err) {
+                                console.log(err)
+                            }else{
+                                req.flash("message","succefully registered please check your email and get the otp")
+                                res.redirect("/otp")
+                            }
+                        })
+                        
                     })
                     
-                })
+                }
                 
             }
-            
         }
+        )
+    } else {
+        req.flash("message","the passwords do not match")
+        res.redirect("/register")
     }
-    )
+   
 }
 module.exports = registerApiLogic
--- conrollers/apis_routes/home.js
+++ conrollers/apis_routes/home.js
@@ -2,13 +2,24 @@ const User = require("../../db_conn/models/users")
 const homeApiController = (app)=>{
     app.route("/")
         .get((req,res)=>{
-            User().find({package:"Gold"},(err,users)=>{
-                if (err) {
-                    console.log(err)
-                } else {
-                    res.render("index",{users:users})
-                }
-            })
+            if(req.isAuthenticated()){
+                User().find({package:"Gold"},(err,users)=>{
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        res.render("index",{users:users,user:req.user,status:"signedup"})
+                    }
+                })
+            }else{
+                User().find({package:"Gold"},(err,users)=>{
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        res.render("index",{users:users,status:"signedout"})
+                    }
+                }) 
+            }
+           
            
         })
 }
