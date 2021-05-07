17b75c20826076b80ca3c76ee99c6573240c4be8
joekingsleymukundi <joekingsleymukundi@gmail.com>
2021-05-07 02:56:47 -0700

    lounching day6

--- mails/options/registerOptiotions.js
+++ mails/options/registerOptiotions.js
@@ -3,7 +3,7 @@ const registerOptions = (email,fullname,otp)=>{
         from:'"GoldlineBreeze" <info@goldlinebreeze.com>',
         to: email,
         subject: "Successfull registration",
-        html: `<p><b>Congraduations and welcome on board ${fullname}</b>🎉🎉🙌<br>.Your one time pin is ${otp}.We first want to welcome you to our community<br>
+        html: `<p><b>Congratulations and welcome on board ${fullname}</b>🎉🎉🙌<br>.Your one time pin is ${otp}.We first want to welcome you to our community<br>
         We are the best option and we are glad you have joined us.<br>We have packages that you can enroll to, More info on our landing page:<br>
         https://www.goldlinebreeze.com<br><br>
         For any clarifications, please  do not hesitate  to contact the  undersigned  via  <b>EMAIL</b><br><br>
