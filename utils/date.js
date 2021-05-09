0000cb3e73d3457676b26f5b84065977c4b2ba51
joekingsleymukundi <joekingsleymukundi@gmail.com>
2021-05-10 02:53:16 +0300

    f23

--- utils/date.js
+++ utils/date.js
@@ -1,10 +1,7 @@
 const date=()=>{
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-
-    today = mm + '/' + dd + '/' + yyyy;
+    var day=new Date();
+    const options= {day:'numeric', month:'long', year:"numeric", timeZone: 'Africa/Nairobi'};
+    const today=day.toLocaleDateString("en-IN", options);
     return today
 }
 module.exports=date
